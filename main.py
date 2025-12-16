63152c10f26f711851a1fd78fd67a9c6ea608b9c
Santi <macchiasantino@gmail.com>
2025-12-15 21:47:23 -0300

    agregamos post de cursos y usuarios

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+from pydantic import BaseModel
 cred = credentials.Certificate("firebase.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -9,14 +9,37 @@ app =FastAPI()
 @app.get("/health")
 def obtener_salud_sitio():
     return "Santinoo"
+class Usuario (BaseModel):
+    nombre:str
+    email:str
+    edad:int
+    contraseña:str
+    repetir_contraseña:str
+class Curso (BaseModel):
+    precio:int
+    nombre:str
+    descripcion:str
+    duracion:int
 
 
 @app.get("/usuarios")
 def obtener_usuarios():
     collection = db.collection("usuarios").stream()
     return [c.to_dict() for c in collection]
-
+@app.post("/usuarios")
+def crear_usuario (usuario:Usuario):
+    if usuario.contraseña != usuario.repetir_contraseña:
+        return "Las contraseñas no coinciden"
+    del usuario.repetir_contraseña
+    db.collection("usuarios").add(usuario.dict())
+    return "usuario creado con exito"
 @app.get("/cursos")
 def obtener_cursos():
     collection = db.collection("cursos").stream()
     return [c.to_dict() for c in collection]
+@app.post("/cursos")
+def crear_curso (curso:Curso):
+    if curso.nombre == "admin":
+        return "el nombre no puede llevar admin"
+    db.collection("cursos").add(curso.dict())
+    return "curso creado con exito"
